c949242e0f1a6c4298997f10293e58385a2af1de
Tomoya Kose <tomoya@mitsuse.jp>
2017-07-19 00:12:09 +0900

    Fill entities of matrix with same value.

--- Tests/LaTests/Size.swift
+++ Tests/LaTests/Size.swift
@@ -6,3 +6,5 @@ enum Zero: Size { static let value: Int = 0 }
 enum Two: Size { static let value: Int = 2 }
 enum Three: Size { static let value: Int = 3 }
 enum Four: Size { static let value: Int = 4 }
+
+enum _100: Size { static let value: Int = 100 }
