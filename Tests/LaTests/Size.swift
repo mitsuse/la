5305b2770887240db63ca79ec90d963234b1b995
Tomoya Kose <tomoya@mitsuse.jp>
2017-07-29 17:21:54 +0900

    Move implementations of `Field` to la-accelerate.
    * la-accelerate: https://github.com/mitsuse/la-accelerate

--- Tests/LaTests/Size.swift
+++
@@ -1,8 +0,0 @@
-import La
-
-enum _0: Size { static let value: Int32 = 0 }
-enum _2: Size { static let value: Int32 = 2 }
-enum _3: Size { static let value: Int32 = 3 }
-enum _4: Size { static let value: Int32 = 4 }
-
-enum _1000: Size { static let value: Int32 = 1000 }
